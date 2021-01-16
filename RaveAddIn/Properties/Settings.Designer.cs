bf0cf9cbf26d14ee3a6b586bfa4fc3eec662efef
Philip <philip@northarrowresearch.com>
2021-01-15 16:04:24 -0800

    Upgrading to ArcGIS 10.6.1

--- RaveAddIn/Properties/Settings.Designer.cs
+++ RaveAddIn/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace RaveAddIn.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("United States")]
         public string BaseMap {
             get {
                 return ((string)(this["BaseMap"]));
