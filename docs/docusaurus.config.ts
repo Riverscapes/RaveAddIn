218b31ef8fc34d330316faa739c225148cca8583
Matt Reimer <matt@northarrowresearch.com>
2025-07-22 13:12:27 -0700

    adding github link

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -111,6 +111,11 @@ const config: Config = {
             { to: '/technical-reference/symbology/symbology-web-rasters', label: 'Web Raster Symbology' },
           ]
         },
+  {
+          href: 'https://github.com/Riverscapes/riverscapes-tools', // External GitHub link
+          label: 'GitHub',
+          position: 'right',
+        },
       ],
     },      
 
@@ -122,101 +127,3 @@ const config: Config = {
 }
 
 export default config
-const menuLinks = [
-      {
-        title: 'About',
-        url: '/about',
-        items: [
-          {
-            title: 'About Riverscapes Viewer',
-            url: '/about',
-          },
-          {
-            title: 'Acknowledgements',
-            url: '/about/acknowledgements',
-          },
-          {
-            title: 'Questions, Feature Requests & Bugs',
-            url: '/about/known-bugs',
-          },
-          {
-            title: 'License & Source Code',
-            url: '/about/license',
-          },
-        ],
-      },
-      {
-        title: 'Deploy',
-        url: '/Deploy/install-qgis',
-        items: [
-          {
-            title: 'Riverscapes Viewer for the Web',
-            url: '/Deploy/install-web',
-          },
-          {
-            title: 'Riverscapes Viewer for QGIS',
-            url: '/Deploy/install-qgis',
-          },
-          {
-            title: 'Riverscapes Viewer for ArcGIS Pro',
-            url: '/Deploy/install-arcpro',
-          },
-        ],
-      },
-      {
-        title: 'Software Help',
-        url: '/software-help',
-        items: [
-          {
-            title: 'Riverscapes Web Viewer',
-            url: '/software-help/help-web',
-          },
-          {
-            title: 'Riverscapes Viewer for QGIS',
-            url: '/software-help/help-qgis',
-          },
-          {
-            title: 'Riversccapes Viewer for ArcGIS Pro',
-            url: '/software-help/help-arcpro',
-          },
-        ],
-      },
-      {
-        title: 'Technical Reference',
-        url: '/technical-reference',
-        items: [
-          {
-            title: 'Base Maps',
-            url: '/technical-reference/base-maps',
-          },
-          {
-            title: 'Business Logic',
-            url: '/technical-reference/business-logic',
-          },
-          {
-            title: 'Project Views',
-            url: '/technical-reference/project-views',
-          },
-          {
-            title: 'Symbology',
-            url: '/technical-reference/symbology',
-          },
-          {
-            title: 'ArcViewer Symbology',
-            url: '/technical-reference/symbology/symbology-arc',
-          },
-          {
-            title: 'QViewer Symbology',
-            url: '/technical-reference/symbology/symbology-qgis',
-          },
-          {
-            title: 'Web Vector Symbology',
-            url: '/technical-reference/symbology/symbology-web-vectors',
-          },
-          {
-            title: 'Web Raster Symbology',
-            url: '/technical-reference/symbology/symbology-web-rasters',
-          }
-        ]
-      }
-    ]
